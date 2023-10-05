3d3d398d3ac5096a88ca97c1ad7345048df4716d
EminatarenX <eminataren2002@gmail.com>
2023-10-05 14:05:54 -0600

    commit

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: `http://localhost:4000/api`
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
 })
 
 export default clienteAxios
