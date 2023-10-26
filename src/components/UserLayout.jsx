f975755becc1d8a3b955fa16860772ba9f3019d8
EminatarenX <eminataren2002@gmail.com>
2023-10-26 15:08:58 -0600

    anadiendo aside y pagina de inicio en userDashboard

--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -1,12 +1,81 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation, Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import logo from '../assets/img/Logo-header.svg'
+// Redux
+import { cerrarSesionAction } from "../actions/usuariosActions"
+import { useDispatch } from "react-redux"
 
 export default function UserLayout() {
+  const location = useLocation()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  // States
+  const [ aside, setAside] = useState(false)
+
+  const cerrarSesion = (e) => {
+    e.preventDefault()
+
+    dispatch(cerrarSesionAction())
+    navigate('/login')
+
+  }
+
   return (
     <>
-    <header>
-        User
-    </header>
-<Outlet />    
-</>
+      <aside 
+        className={`fixed  top-0 ${aside ? 'left-0' : '-left-[1000px]'} transition-all duration-700 h-screen w-full bg-emerald-900  flex flex-col lg:hidden items-center gap-10 justify-center p-5`}
+      >
+              <button>
+                
+              </button>
+              <Link to={'/'}>
+                <img src={logo} className='h-12' alt="logo" />
+              </Link>
+              <Link className="text-white text-lg hover:scale-105 transition-transform" to={'/settings'}>
+                Configuración
+              </Link>
+              <button className="text-white text-lg hover:scale-105 transition-transform" onClick={cerrarSesion}>
+                Cerrar Sesión
+              </button>
+         
+        
+      </aside>
+      <header className="flex lg:hidden bg-emerald-800 p-3 ">
+        <button className="text-white text-2xl z-10"
+          onClick={() => {
+            setAside(!aside)
+          }}
+        >
+          &#9776;
+        </button>
+      </header>
+      <main className="flex">
+
+        <aside className="w-1/5 bg-emerald-800 h-screen hidden lg:flex flex-col gap-10 p-5 ">
+          <h1 className="text-emerald-100 text-4xl capitalize">{location.pathname === "/dashboard" ? "Bienvenido" : location.pathname.split('/')[1]}</h1>
+          <Link to={'/dashboard'} className="relative group text-start text-emerald-100 capitalize text-xl overflow-hidden">
+            Inicio
+            <span className="absolute inset-x-0 w-0 bottom-0 h-0.5 bg-white group-hover:w-full transition-all"></span>
+          </Link>
+          <Link to={'/settings'} className="relative group text-start text-emerald-100 capitalize text-xl overflow-hidden">
+            Configuración
+            <span className="absolute inset-x-0 w-0 bottom-0 h-0.5 bg-white group-hover:w-full transition-all"></span>
+          </Link>
+          <button 
+            onClick={cerrarSesion}
+            className="relative group text-start text-emerald-100 capitalize text-xl overflow-hidden">
+            Cerrar sesion
+            <span className="absolute inset-x-0 w-0 bottom-0 h-0.5 bg-white group-hover:w-full transition-all"></span>
+          </button>
+
+        </aside>
+        <main className="w-full lg:w-4/5 bg-white-500 p-5 bg-slate-200">
+
+          <Outlet />
+        </main>
+      </main>
+
+    </>
   )
 }
--- src/pages/user/UserDashboard.jsx
+++ src/pages/user/UserDashboard.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import {Fragment} from 'react'
 
 export default function UserDashboard() {
     
   return (
-    <div>UserDashboard</div>
+      <section className='grid grid-cols-2 gap-2'>
+        <article className='bg-white rounded p-2 col-span-2'>
+        <h2 className='text-4xl text-emerald-900'>Registrar</h2>
+
+        <div className='flex gap-5 mt-5 flex-col'>
+          <button className='bg-emerald-200 border border-emerald-600 rounded p-10 text-emerald-900 w-full font-semibold text-2xl'>
+              Entrada
+          </button>
+          <button className='bg-red-100 border border-red-600 rounded p-10 text-red-900 w-full font-semibold text-2xl'>
+            Salida
+          </button>
+        </div>
+      </article>
+      <article className='bg-white rounded p-2 '>
+        <h2 className='text-2xl text-emerald-900 font-semibold'>Plaza</h2>
+      </article>
+      <article className='bg-white rounded p-2 '>
+        <h2 className='text-2xl text-emerald-900 font-semibold'>Empresa</h2>
+      </article>
+
+      <article className='bg-white rounded p-5 col-span-2'>
+        <h2 className='text-2xl font-semibold text-emerald-900'>Resumen</h2>
+      </article>  
+    </section>
   )
 }
