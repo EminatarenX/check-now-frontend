27f840cbbfffc9779452549bd5f21ca1aaa1a619
Eminataren <eminataren2002@gmail.com>
2023-11-23 08:53:34 -0600

    cambiando el string tarde

--- src/pages/user/UserIndex.jsx
+++ src/pages/user/UserIndex.jsx
@@ -24,7 +24,7 @@ export default function UserIndex() {
       let entradaObject = {comentarios: ''}
       
       
-      if(hora <= horaEntrada){
+      if(hora >= horaEntrada){
         if(minutes > (minutosEntrada + 15)){
           entradaObject.comentarios = 'Tarde'
         }else {
